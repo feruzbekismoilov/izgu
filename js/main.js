a57ab9559a38c9cba51ee42609e45f30b4f83adf
feruzbekismoilov <ismoilovferuz0858@gmail.com>
2023-04-01 23:48:34 +0500

    add node-modules and change css

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ elChooseLanguages.addEventListener("click", (evt)=>{
     document.querySelector(".header__inner__end__languages__icon").classList.toggle("show-icon");
     document.querySelector(".header__show-list").classList.toggle('header__visually__list');
 })
+
+
+//  const a = "123456789"
+
+//  const s=[...a].reduce((ele,summ)=>ele+summ)
+//  console.log(s);
