ba38f3b458c1af0c30e8c7506f2b68c07cec2258
feruzbekismoilov <ismoilovferuz0858@gmail.com>
2023-04-03 08:49:09 +0500

    add js to header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// ------------------------ CHANGE LANGUAGES START ---------------------------------------------
+
 const elChooseLanguages = document.querySelector(".header__inner__end__languages");
 elChooseLanguages.addEventListener("click", (evt)=>{
     evt.preventDefault();
@@ -5,9 +7,30 @@ elChooseLanguages.addEventListener("click", (evt)=>{
     document.querySelector(".header__inner__end__languages__icon").classList.toggle("show-icon");
     document.querySelector(".header__show-list").classList.toggle('header__visually__list');
 })
+// ------------------------ CHANGE LANGUAGES END ------------------------------------------------
 
 
-//  const a = "123456789"
+// ------------------------- FOR HEADER SCROLL START -----------------------------------------------------
 
-//  const s=[...a].reduce((ele,summ)=>ele+summ)
-//  console.log(s);
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+     document.querySelector(".header").style.backgroundColor = "white";
+     document.querySelector(".header").style.borderBottomStyle = "#fafafa";
+     document.querySelector(".header__inner__end__languages").style.backgroundColor = "#EEF0F2";
+     document.querySelector(".header__inner__end__btn").style.backgroundColor = "#0D9676";
+     document.querySelector(".header__inner__end__btn").style.color = "#fff";
+    document.querySelector(".header__inner__link__logo").src = "../images/icons/green-logo.svg";
+  }else {
+    document.querySelector(".header").style.backgroundColor = "";
+    document.querySelector(".header").style.borderBottomStyle = "";
+    document.querySelector(".header__inner__end__languages").style.backgroundColor = "";
+    document.querySelector(".header__inner__end__btn").style.backgroundColor = "";
+    document.querySelector(".header__inner__end__btn").style.color = "";
+    document.querySelector(".header__inner__link__logo").src = "../images/icons/white-logo.svg";
+
+
+  }
+}
+// -------------------------- FOR HEADER SCROLL END ---------------------------------------------------
